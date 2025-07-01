f2230345790836225897440dcb432d52a4d5931c
Student <budhathokiresima@gmail.com>
2025-07-01 22:41:05 +0545

    random num generator

--- random_num_gen/script.js
+++ random_num_gen/script.js
@@ -1,2 +1,6 @@
 const randomNumberElement = document.getElementById('randomNumber');
 const generateButton = document.getElementById('generate');
+generateButton.addEventListener('click', () => {
+    const randomNumber = Math.floor(Math.random() * 100) + 1; // Generates a random number between 1 and 100
+     randomNumberElement.textContent = randomNumber;
+});
