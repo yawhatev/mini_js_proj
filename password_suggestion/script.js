25537aa29457567cdfbf3b25ede39b246f69f033
Student <budhathokiresima@gmail.com>
2025-07-04 17:53:59 +0545

    done with password suggestion

---
+++ password_suggestion/script.js
@@ -0,0 +1,15 @@
+const suggestBtn = document.getElementById('suggestBtn');
+const passwordInput = document.getElementById('passwordInput');
+
+suggestBtn.onclick=function() {
+    const length = 8; 
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+    let password = "";
+    
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    
+    passwordInput.value = password;
+};
